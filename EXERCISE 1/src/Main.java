b2c9467eb0973ae08903a920951f9dac72a8c9ad
Camilo Valdes <camiloga17310@gmail.com>
2022-08-11 12:48:37 -0300

    ver1.0

---
+++ EXERCISE 1/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        suma(2,4,6);
+    }
+    public static void suma(int a,int b,int c){
+        int result;
+        result = a + b + c;
+        System.out.println(result);
+    }
+
+}
+
